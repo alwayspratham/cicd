4ee704424cd463301f35fb012a898b3583aa2de6
alwayspratham <prathamkoparde@gmail.com>
2026-04-28 12:19:48 +0530

    added name route

--- app/main.py
+++ app/main.py
@@ -16,6 +16,6 @@ def read_root():
 def health_check():
     return {"status": "ok"}
 
-@app.get("/name/{name}")
-def show(name: str):
-    return {"name": f"name is {name}"}
+@app.get("/name/{name}{age}")
+def show(name: str,age:int):
+    return {"name": f"name is {name} age is {age}"}
